086366bf2d1ed9ada9b0f1d24e447fd34f18512a
MaxPrehoda <maxprehoda@gmail.com>
2022-10-29 13:32:03 -0700

    gave MyApp AppProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import React from 'react'
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 

331e0190bb965b1461e974771d797b2b3d9955d2
MaxPrehoda <maxprehoda@gmail.com>
2022-11-29 10:46:29 -0800

    FEAT: custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Custom404() {
+    return(<main className="bg-neutral-800 w-screen h-screen">
+        <h1 className="text-pink-400">404 - Page Not Found</h1>
+    </main>
+  )}

d7f32476aa2ecec3d62fd6f4d1858587e081a3b7
MaxPrehoda <maxprehoda@gmail.com>
2022-11-28 21:27:09 -0800

    FEAT: UserStats Component and Route

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,4 +1,11 @@
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
+
+//generate a random number between 0 and 62
+
+const randomNum = () => {
+    return Math.floor(Math.random() * 62)
+}
 
 function UserList() {
   const [data, setData] = useState<any[]>([])
@@ -22,7 +29,14 @@ function UserList() {
     // make a list of users with className='w-[300px] h-[25px] pb-2 mb-2 bg-white rounded-md' key={user.id}>{user.username}
     <ul>
       {data.map((user) => (
-        <li className='w-[300px] h-[30px] pb-2 mb-2 bg-zinc-700 rounded-md pl-2 pt-1' key={user.id}>{user.username}</li>
+        <Link href='/userStats'>
+        <li className='relative w-[300px] h-[40px] pb-2 mb-3 bg-neutral-700 rounded-md pl-2 pt-2' key={user.id}>{user.username}<p className='absolute right-0 pr-2 -mt-6'><span>
+            {randomNum()}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="-mt-6 ml-5 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                  </svg>
+        </span>
+      </p></li></Link>
       ))}
     </ul>
   )
--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -46,7 +46,24 @@ export default function leaderBoard() {
       </Head>
 
       <main className={styles.main+' bg-neutral-900'}>
-      <div className="sm:mb-8 sm:flex sm:justify-center">
+      <div className="flex-col justify-center space-y-2">
+      <span className="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="inline w-8 h-8 pb-1.5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+          />
+        </svg>
+        <h1 className="text-2xl inline text-white pl-1">Leaderboard</h1>
+      </span>
     <UserList />
     </div>
       </main>

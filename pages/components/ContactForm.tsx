200e64ee88c2a69a9dd1c5c93d799ff7b629ea80
asa-leholland <asaleholland@gmail.com>
2022-12-01 00:18:25 -0800

    WIP: form submission for contact email

--- pages/components/ContactForm.tsx
+++ pages/components/ContactForm.tsx
@@ -12,7 +12,7 @@ const ContactForm = () => {
             email: email.value,
             message: message.value,
         };
-        let response = await fetch("http://localhost:3000/contact", {
+        let response = await fetch("http://localhost:3000/api/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
@@ -25,19 +25,21 @@ const ContactForm = () => {
     };
     return (
         <form onSubmit = { handleSubmit } >
+
         <div >
+        <label className="text-3xl font-semibold leading-8 pl-2 text-white">Contact Us</label>
             <div className="relative gap-6 sm:flex-row md:flex-col lg:flex-row">
                 <div>
                     <label className="text-lg font-semibold leading-8 pl-2 text-white">Name </label>
-                    <input className="mt-2 text-base leading-7 text-white" type = "text" id = "name" required />
+                    <input className="mt-2 text-base leading-7" type = "text" id = "name" required />
                 </div>
                 <div>
                     <label className="text-lg font-semibold leading-8 pl-2 text-white">Email </label>
-                    <input className="mt-2 text-base leading-7 text-white" type = "text" id = "email" required />
+                    <input className="mt-2 text-base leading-7" type = "text" id = "email" required />
                 </div>
                 <div>
                     <label className="text-lg font-semibold leading-8 text-white">Message </label>
-                    <textarea className="mt-2 text-base leading-7 text-white" id = "message" required />
+                    <textarea className="mt-2 text-base leading-7" id = "message" required />
                 </div>
             <button className="text-lg border-solid border-2 pl-14 pr-14 ml-10 mr-10 border-indigo-600 font-semibold leading-8 text-white" type = "submit" > { status } </button>
             </div>

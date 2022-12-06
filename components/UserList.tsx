ef79c0b7b98a56fdd7f45713fd6e78367df01515
MaxPrehoda <maxprehoda@gmail.com>
2022-12-05 16:09:31 -0800

    FIX: deployment key error silenced

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -23,12 +23,14 @@ function UserList() {
 
   if (isLoading) return <p>Loading...</p>
   if (!data) return <p>No profile data</p>
+
+  console.log(data)
   
   return ( 
     // make a list of users with className='w-[300px] h-[25px] pb-2 mb-2 bg-white rounded-md' key={user.id}>{user.username}
     <ul>
       {data.map((user) => (
-        <Link href={'/userStats/' + user.username}>
+        <Link href={'/userStats/' + user.username} key={user.id}>
         <li className='relative w-[300px] h-[40px] pb-2 mb-3 bg-neutral-700 rounded-md pl-2 pt-2' key={user.id}>{user.username}<p className='absolute right-0 pr-2 -mt-6'><span>
             {randomNum()}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="-mt-6 ml-5 w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />

084f5e89335286486a936a8f9de24d969e7d75bf
MaxPrehoda <maxprehoda@gmail.com>
2022-12-20 02:26:54 -0800

    FIX: &apos; instead of '

--- pages/components/ContactForm.tsx
+++ pages/components/ContactForm.tsx
@@ -29,7 +29,7 @@ const ContactForm = () => {
         <div className="relative w-[800px] h-[450px] bg-neutral-800 rounded-lg p">
         <label className="text-3xl font-semibold leading-8 pl-12 pb-1 mt-12 absolute text-white">Get in touch</label>
         <p className="absolute pt-12 w-[40%] ml-12 mt-12 font-thin">
-We value your feedback, so please don't hesitate to reach out to us with any questions, comments, or suggestions you might have about our app. We'd love to hear from you!
+We value your feedback, so please don&apos;t hesitate to reach out to us with any questions, comments, or suggestions you might have about our app. We&apos;d love to hear from you!
 If you need technical support, please include as much information about your system/enviroment as possible when submitting a ticket.
 We look forward to hearing from you!</p>
         <div className="bg-transparent left-12 top-72 border-[1px] border-pink-300 rounded-md w-[17.5%] h[2%] text-zinc-50 absolute invisible hover:block"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="mr-2 inline w-6 h-6">
